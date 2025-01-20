184e9c0c727c6fe2428be2396692d87191adb61a
Jiaqi Liu ｜【烬火】胡桃 <jack20220723@gmail.com>
2025-01-20 15:38:12 +0800

    Declare successful PoC and prepare for long-term development (#10)

---
+++ packages/alexandria-app/src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2025 Jiaqi Liu. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import "./Dashboard.css";
+import GitHubSection from "./GitHubSection";
+
+import Greek from "./greek.svg";
+import Latin from "./latin.svg";
+import Hebrew from "./hebrew.svg";
+import Sanskrit from "./sanskrit.svg";
+import avatar from "./avatar.png";
+
+function Dashboard(): JSX.Element {
+  return (
+    <div className="dashboard">
+      <div className="project">
+        <div className="imageCropper">
+          <img src={avatar} alt="" />
+        </div>
+        <h3>Alexandria</h3>
+        <p>Grammar</p>
+      </div>
+      <div className="links">
+        <div className="link">
+          <div className="icon">
+            <img src={Greek} alt="Greek" />
+          </div>
+          <h2>Ancient Greek</h2>
+        </div>
+        <div className="link">
+          <div className="icon">
+            <img src={Latin} alt="Latin" />
+          </div>
+          <h2>Latin</h2>
+          <h3>Coming Soon</h3>
+        </div>
+        <div className="link">
+          <div className="icon">
+            <img src={Hebrew} alt="Hebrew" />
+          </div>
+          <h2>Classical Hebrew</h2>
+          <h3>Coming Soon</h3>
+        </div>
+        <div className="link">
+          <div className="icon">
+            <img src={Sanskrit} alt="Sanskrit" />
+          </div>
+          <h2>Classical Sanskrit</h2>
+          <h3>Coming Soon</h3>
+        </div>
+      </div>
+      <GitHubSection />
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ packages/alexandria-app/src/components/dashboard/GitHubSection.tsx
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2025 Jiaqi Liu. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import "./GitHubSection.css";
+import { useState } from "react";
+
+import github from "./github.png";
+
+function GitHubSection(): JSX.Element {
+  const [isVisible, setIsVisible] = useState<boolean>(true);
+
+  const handleClick = (): void => {
+    setIsVisible(false);
+
+    // This side effect is used to make the GH button fade back in on browser backward operation
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 2000);
+  };
+
+  return (
+    <>
+      <a
+        href="https://github.com/QubitPi/alexandria"
+        onClick={handleClick}
+        style={{
+          textDecoration: "none",
+          opacity: isVisible ? 1 : 0,
+          transition: "opacity 1.0s ease-in-out",
+        }}
+      >
+        <div className={`github ${isVisible ? "" : "up"}`}>
+          <h2>Fork it on GitHub</h2>
+          <img src={github} width="80%" alt="" />
+        </div>
+      </a>
+    </>
+  );
+}
+
+export default GitHubSection;
